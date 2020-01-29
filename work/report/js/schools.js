c392deb5a9735349e04748c39606bfa34a98f7df
kjronning <kjronning@gmail.com>
2020-01-29 11:39:07 +0100

    added table to schools.html and table population code to schools.js

--- work/report/js/schools.js
+++ work/report/js/schools.js
@@ -1,75 +1,47 @@
 
-
-
 const getFetchURI = function(municipalityCode){
     return fetchBaseURI+municipalityCode;
 };
 
-const fetchMunicipalitySchools = function(municipalityCode){
-  fetch(getFetchURI(municipalityCode)).then((response)=> {
-      return response.json();
-  })
-};
-
 const fetchMunicipalities = function(){
-    fetch(fetchBaseURI).then((response) => {
-        return response.json();
-    })
+    fetch(fetchBaseURI).then((data)=> {
+        let option;
+        console.log("municipality length: " + data.length);
+        for (let i=0; i<data.length; i++){
+            option = document.createElement('option');
+            option.text = data.namn;
+            option.value = data.kommunkod;
+            municipalityList.add(option);
+        }})
 };
 
-const loadSchoolsToList = function(){
-    let municipalityCode = municipalityList.options[municipalityList.selectedIndex].value;
-    let option;
-    let data = fetchMunicipalitySchools(municipalityCode);
-    resetSchoolList();
-    for (let i=0; i<data.length;i++){
-        option = document.createElement('option');
-        option.text = data[i].Skolenhetsnamn;
-        option.value = data[i].Skolenhetskod;
-        schoolList.add(option);
-    }
+const populateTable =  function(municipalityCode){
+    fetch(getFetchURI(municipalityCode)).then((data) =>
+    {
+    for (let i=0;data.length;i++){
+        let row = table.insertRow(i);
+        let schoolCodeCell = row.insertCell(0);
+        let schoolNameCell = row.insertCell(1);
+        let PeOrgNrCell = row.insertCell(2);
+        schoolCodeCell.innerHTML = data[i].Skolenhetskod;
+        schoolNameCell.innerHTML = data[i].Skolenhetsnamn;
+        PeOrgNrCell.innerHTML = data[i].PeOrgNr;
+    }})
 };
 
 const fetchBaseURI = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/";
 
-
+const table = document.getElementById("table");
 const municipalityList = document.getElementById("municipalityList");
 municipalityList.length = 0;
 let defaultOption = document.createElement('option');
 defaultOption.text = "Choose Municipality";
 municipalityList.add(defaultOption);
 municipalityList.selectedIndex = 0;
-let municipalityData = fetchMunicipalities();
-let municipalityOption;
-console.log("municipality length: " + municipalityData.length);
-for (let i=0;i<municipalityData.length;i++){
-    municipalityOption = document.createElement('option');
-    municipalityOption.text = municipalityData.namn;
-    municipalityOption.value = municipalityData.kommunkod;
-    municipalityList.add(municipalityOption);
-    console.log(municipalityOption.text);
-}
+fetchMunicipalities();
 municipalityList.addEventListener("change", (e) => {
     if(e.selectedIndex !== 0){
-        getSchoolsButton.enable();
-        loadSchoolsToList();
-    }else{
-        getSchoolsButton.disable();
-        resetSchoolList();
+        populateTable(municipalityList.options[municipalityList.selectedIndex].value)
     }
 });
 
-const schoolList = document.getElementById("schoolList");
-defaultOption.text = "Choose school";
-const resetSchoolList = function(){
-    schoolList.empty();
-    schoolList.length = 0;
-    schoolList.add(defaultOption);
-    schoolList.selectedIndex = 0;
-};
-resetSchoolList();
-
-const getSchoolsButton = document.getElementById("schoolButton");
-
-
-getSchoolsButton.onclick(loadSchoolsToList);
