976307c83789ae79a2b5b44b5c6de9d4b27b1f8a
kjronning <kjronning@gmail.com>
2020-03-12 19:11:01 +0100

    added create and update.

---
+++ work/report/php/about.php
@@ -0,0 +1,20 @@
+<?php
+include '../view/header.php';
+?>
+<article>
+
+<h1>About this website</h1>
+
+<p>This website is part of the course websoft <a href="https://www.hkr.se/en/course/DA377B">DA377B</a>.</p>
+
+<p>In this course we will learn all about web development, and that's why we're creating a website.</p>
+
+    <p><img src="../img/github.png" width="50" height="50" alt="github icon"/>
+        Checkout the course github repository <a href="https://github.com/Webbprogrammering/websoft/" target="_blank">here.</a><br/></p>
+    <p><img src="../img/fork.png" width="50" height="50" alt="fork icon"/>
+        Also checkout my fork of the repository <a href="https://github.com/Kjronning/websoft" target="_blank">here.</a></p>
+
+</article>
+<?php
+include '../view/footer.php';
+?>
---
+++ work/report/php/me.php
@@ -0,0 +1,22 @@
+
+<?php
+include '../view/header.php';
+?>
+<article>
+<h1>About me</h1>
+
+<p><img src="../img/me.jpg" width="500" alt="Me on an image"></p>
+
+<p>I'm Joakim Rönning. I was born in Avesta, Sweden, but grew up in Concepción, Chile. I've been into computers since I was but a wee boy. As an adult I've done mostly studying; I studied aerospace engineering (not finished), gastronomy (finished), and now software engineering (finishing). I've worked in restaurants, events and bakeries. Now I'm in Sweden ready for the next step in my life.</p>
+
+<p>I learned programming in high school, doing C++. I've come pretty far, finally forgetting everything I learned there.</p>
+
+<p>I recently got into powerlifting, and even though my lifts are quite light I have found myself really enjoying it.</p>
+
+<p>Catch me around</p>
+
+</article>
+
+<?php
+include '../view/footer.php';
+?>
