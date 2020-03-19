05782a4cc8d29f6be5be0d124c014c1a423646a9
kjronning <kjronning@gmail.com>
2020-03-19 22:59:20 +0100

    finished 4,5 and 6 reports.

---
+++ work/s05/view/footer.php
@@ -0,0 +1,10 @@
+<?php ?>
+
+        <script src="../js/duck.js"></script>
+<footer>
+    <hr>
+    <p>Something's a foot...er.</p>
+</footer>
+</div>
+</body>
+</html>
---
+++ work/s05/view/header.php
@@ -0,0 +1,34 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>This is me</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../favicon.ico">
+    <link rel="stylesheet" href="../css/variables.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul class="nav_list">
+            <li class="nav_item">
+                <a href="me.php">Me</a>
+            </li>
+            <li class="nav_item">
+                <a href="report.php">Report</a>
+            </li>
+            <li class="nav_item">
+                <a href="about.php">About</a>
+            </li>
+            <li class="nav_item">
+                <a href="websoftdatabase.php">Search</a>
+            </li>
+            <li class="nav_item">
+                <a href="projects.php">Projects</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<div class="main">
